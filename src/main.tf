01ebd063f7ec3e737d49eeacc28c005e241f52eb
jhonas8 <love.android62@gmail.com>
2024-02-27 00:25:22 -0300

    codigo aula 7

---
+++ src/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 0.13.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.38.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      managed-by = "terraform"
+    }
+  }
+}
